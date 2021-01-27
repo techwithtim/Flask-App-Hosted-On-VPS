b73c1b6880d792f86c2c226f824e8b11610fac26
Tim Ruscica <35348871+techwithtim@users.noreply.github.com>
2021-01-27 17:35:48 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app['SECRET_KEY'] = "secretkey123"
+
+@app.route("/")  # this sets the route to this page
+def home():
+	return jsonify({'Message': "Testing"})
+
+if __name__ == "__main__":
+    app.run()
